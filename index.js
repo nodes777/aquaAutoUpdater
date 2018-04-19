781d23d5588a2dce729833366cbd122efe55d166
Taylor <taylor.nodell@gmail.com>
2018-04-19 15:46:29 -0700

    completes updater

--- index.js
+++ index.js
@@ -11,31 +11,77 @@ var app = admin.initializeApp({
 var db = admin.database();
 var usersRef = db.ref("users");
 
+var users;
+var promises = [];
+
 usersRef.once("value", function(snapshot) {
-  var users = snapshot.val();
-
+  users = snapshot.val();
+  
   for(user in users){
-  	let player = users[user];
-  	// make a stats object / Get stats object
-  	if (player.stats == undefined || player.stats == null) {
-  		player.stats = {};
-  	}
+    let player = users[user];
 
-  	var today = getToday();
-  	// Record the day's agg stats
-  	player.stats[today] = player.portfolio.aggStats;
+    // make a stats object / Get stats object
+    if (player.stats == undefined || player.stats == null) {
+      player.stats = {};
+    }
 
-  	// Add timestamp
-  	var timestamp = Date.now();
-  	player.stats[today].timestamp = timestamp;
-  	console.log(player.stats[today])
+    var today = getToday();
+    //Record the day's agg stats
+    player.stats[today] = player.portfolio.aggStats;
 
-  	// Save in a set method
-  	//console.log(db.ref(`users/${user}/stats`));
-  	db.ref(`users/${user}/stats/${today}`).set(player.stats[today]);
+    // Add timestamp
+    var timestamp = Date.now();
+    player.stats[today].timestamp = timestamp;
+    console.log(player.stats[today])
 
+    // Save in a set method
+
+    var promise = db.ref(`users/${user}/stats/${today}`).set(player.stats[today])
+                  .then(function(){console.log(`Finished setting ${player.username}`)});
+    // Push the promise
+    promises.push(promise);
   }
-})// .then(function(){
+
+  // When all promises have resolved, exit node
+  Promise.all(promises)
+  .then(function(p){
+    console.log(`All promises: \n ${p}`)
+    console.log("Exiting");
+    process.exit(0)
+  }).catch(function (error) {
+    console.error("Error after promises: "+error)
+    process.exit(1)
+  });
+});
+
+
+
+// usersRef.once("value", function(snapshot) {
+//   var users = snapshot.val();
+
+//   for(user in users){
+//     let player = users[user];
+//     // make a stats object / Get stats object
+//     if (player.stats == undefined || player.stats == null) {
+//       player.stats = {};
+//     }
+
+//     var today = getToday();
+//     // Record the day's agg stats
+//     player.stats[today] = player.portfolio.aggStats;
+
+//     // Add timestamp
+//     var timestamp = Date.now();
+//     player.stats[today].timestamp = timestamp;
+//     console.log(player.stats[today])
+
+//     // Save in a set method
+//     //console.log(db.ref(`users/${user}/stats`));
+//     db.ref(`users/${user}/stats/${today}`).set(player.stats[today]);
+
+//   }
+// })
+// .then(function(){
 // 	process.exit(0)
 // })
 
